b7e9b7d589411daf49cde0b3dbcddd30572ef3c5
Yu Osada <osada@shisui.tokyo>
2026-05-08 16:20:10 +0900

    構造体の作成

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-fn main() {
-    let s = String::from("hello world");
-
-    let word = first_word(&s);
-
-    println!("The first word is: {}", word);
+// 構造体 意味のあるグループを形成する複数の関連した値をまとめ、名前づけできる独自のデータ型
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
+fn main() {
+    let mut user1 = build_user(
+        String::from("yamada@example.com"),
+        String::from("Taro Yamada"),
+    );
 
-    for (i, &item)  in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
+    user1.email = String::from("anotheremail@example.com");
+
+    println!("ユーザー名: {}", user1.username);
+    println!("メールアドレス: {}", user1.email);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username, // usernameとemail引数は同じ名前なので、フィールド初期化省略記法を使用可能
+        email,
+        sign_in_count: 1,
     }
-
-    &s[..]
 }

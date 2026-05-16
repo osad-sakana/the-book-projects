5515ab3eb62c54e775e2ee3ca1f9e81a52a60154
Yu Osada <osada@shisui.tokyo>
2026-05-16 18:14:20 +0900

    構造体とEnumを組み合わせる

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
     };
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("Home IP address: {}", home.address);
+    println!("Loopback IP address: {}", loopback.address);
 }

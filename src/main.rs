9025d7a31e0d685d89b0cfbee707a036113677c8
Yu Osada <osada@shisui.tokyo>
2026-05-17 10:12:44 +0900

    Enumのメソッド作成

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-enum IpAddr{
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) -> String {
+        match self {
+            Message::Quit => String::from("Quit"),
+            Message::Move { x, y } => format!("Move to ({}, {})", x, y),
+            Message::Write(text) => format!("Write: {}", text),
+            Message::ChangeColor(r, g, b) => format!("Change color to RGB({}, {}, {})", r, g, b),
+        }
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let msg1 = Message::Quit;
+    let msg2 = Message::Move { x: 10, y: 20 };
+    let msg3 = Message::Write(String::from("Hello, Rust!"));
+    let msg4 = Message::ChangeColor(255, 0, 0);
 
-    println!("無事にインスタンスが作成されました。");
+    println!("{}", msg1.call());
+    println!("{}", msg2.call());
+    println!("{}", msg3.call());
+    println!("{}", msg4.call());
 }

659a69c3b380d95fb6f13250426b825dea9b0284
Yu Osada <osada@shisui.tokyo>
2026-05-21 12:14:52 +0900

    モジュール化

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,10 @@
-enum Coin {
-    OneYen,
-    FiveYen,
-    TenYen,
-    FiftyYen,
-    HundredYen,
-    FiveHundredYen,
-}
+use crate::garden::vegetables::Asparagus;
 
-impl Coin {
-    fn value(&self) -> u32 {
-        match self {
-            Coin::OneYen => 1,
-            Coin::FiveYen => 5,
-            Coin::TenYen => 10,
-            Coin::FiftyYen => 50,
-            Coin::HundredYen => 100,
-            Coin::FiveHundredYen => 500,
-        }
-    }
-}
+pub mod garden;
 
-fn main(){
-    let coin_list = [
-        Coin::OneYen,
-        Coin::FiveYen,
-        Coin::TenYen,
-        Coin::FiftyYen,
-        Coin::HundredYen,
-        Coin::FiveHundredYen,
-    ];
-
-    for coin in &coin_list {
-        println!("The value of the coin is: {} yen", coin.value());
-    }
-    let total_value: u32 = coin_list.iter().map(|coin| coin.value()).sum();
-
-    println!("The total value of all coins is: {} yen", total_value);
+fn main() {
+    let plant = Asparagus {
+        name: "アスパラガス",
+    };
+    println!("This is an asparagus: {:?}", plant);
 }

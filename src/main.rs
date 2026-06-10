1e5b2fe5536e5666a31cc7e6f6d3fcee56c97cc0
Yu Osada <osada@shisui.tokyo>
2026-06-10 16:00:22 +0900

    要素外にアクセスしてパニックを起こす

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 fn main() {
     let v = vec![1, 2, 3, 4, 5];
 
-    let third: Option<&i32> = v.get(2);
-    match third {
-        Some(third) => println!("The third element is {third}"),
-        None => println!("There is no third element."),
-    }
+    let does_not_exist = &v[100];
+    println!("{:?}", does_not_exist);
+
+    let does_not_exist = v.get(100);
+    println!("{:?}", does_not_exist);
 }

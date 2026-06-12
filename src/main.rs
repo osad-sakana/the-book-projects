d7c061d38d31d830b9d89ad436e65313399493d4
Yu Osada <osada@shisui.tokyo>
2026-06-12 15:01:25 +0900

    Enum型のベクトルにする方法

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main(){
-    let mut v = vec![100, 32, 57];
-
-    for i in &mut v {
-        *i += 50;
+    enum SpreadsheetCell{
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
 
-    println!("{:?}", v);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }

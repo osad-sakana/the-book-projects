015de8ff9131b4713231e9fcef89b91502853e2d
Yu Osada <osada@shisui.tokyo>
2026-06-12 14:57:45 +0900

    for文でベクトルの1つずつの値を変更する

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+fn main(){
+    let mut v = vec![100, 32, 57];
 
-
-fn main() {
-    let v = vec![1, 2, 3];
-
-    for i in &v{
-        println!("{}", i);
+    for i in &mut v {
+        *i += 50;
     }
+
+    println!("{:?}", v);
 }

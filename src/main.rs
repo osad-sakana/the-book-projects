2fb1cbf7b464df42563872bc9adf1a31bce7a7b7
Yu Osada <osada@shisui.tokyo>
2026-07-19 10:00:23 +0900

    panicの例

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 fn main(){
-    enum SpreadsheetCell{
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
+    let v = vec![1, 2, 3, 4, 5];
 
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
+    v[99];
 }

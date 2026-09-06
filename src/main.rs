ef68da12de8e1079a1e4af0408729bc8f29945a8
Yu Osada <osada@shisui.tokyo>
2026-09-06 10:36:02 +0900

    expectで書く方法

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("ファイルを作成できませんでした: {:?}", error);
-            })
-        } else {
-            panic!("ファイルを開けませんでした: {:?}", error);
-        }
-    });
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txtはプロジェクトに含まれているべきです。");
 }

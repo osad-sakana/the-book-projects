09ff6e8c24f2e0030dd6c46d3e914aaa5c4b4626
Yu Osada <osada@shisui.tokyo>
2026-09-12 14:24:16 +0900

    関数化

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::fs::File;
-use std::io::{self, Read};
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("hello.txt");
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    // ?演算子はエラーが発生した場合に即座に関数からエラーを返すため、前のmatch文と同じ効果を持つ
-    // ただし、?演算子はResult型の値に対してのみ使用できるため、Result型の値を返す関数でのみ使用可能。
-    Ok(username)
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }

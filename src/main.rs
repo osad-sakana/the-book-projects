39d81bae1076a06e324cd7c44610139fd8498583
Yu Osada <osada@shisui.tokyo>
2026-09-20 10:09:21 +0900

    ライフタイム指定子の最初

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,13 @@
-// Trait(共通の振る舞い)を定義する
-pub trait Summary {
-    fn summarize(&self) -> String {
-        String::from("(もっと読む)")
-    }
-    fn get_author(&self) -> String;
-}
-
-// データ構造を定義する
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-// SummaryトレイトをNewsArticleとTweetに実装する
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-
-    fn get_author(&self) -> String {
-        format!("{}", self.author)
-    }
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("@{}: {}", self.username, self.content)
-    }
-
-    fn get_author(&self) -> String {
-        format!("{}", self.username)
-    }
-}
-
 fn main() {
-    let article = NewsArticle {
-        headline: String::from("Rustはいいね"),
-        location: String::from("東京"),
-        author: String::from("田中"),
-        content: String::from("Rustは非常に便利です！"),
-    };
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    let tweet = Tweet {
-        username: String::from("佐藤"),
-        content: String::from("今日の天気はいいですね！"),
-        reply: false,
-        retweet: false,
-    };
+    let result = longest(string1.as_str(), string2);
 
-    println!("{}", article.summarize());
-    println!("{}", tweet.summarize());
+    println!("The longest string is {}", result);
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    // 返しているstrのライフタイムがこの段階ではxに依存するのか、yに依存しているのかコンパイラにはわからないため、ライフタイムを明示する必要がある
+    if x.len() > y.len() { x } else { y }
 }
